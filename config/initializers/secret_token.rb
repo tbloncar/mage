f170a341375b1866efddccce2718d8b1314d8e6b
Travis Loncar <loncar.travis@gmail.com>
2013-04-16 18:10:39 -0500

    Added Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mage::Application.config.secret_token = 'e5914dd561a578e4d59b88609719e64532308bbf7c91b811efa2e53a47ebd0db83225f4c36fb94d899efbdff3d64f1c62e3f33a9291bdb619dd607b614080a0f'
