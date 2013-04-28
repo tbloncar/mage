b6698428cf307dd54fcdbcbbc0a2a6016e29709a
Travis Loncar <loncar.travis@gmail.com>
2013-04-28 01:54:53 -0500

    Added uniqueness validation for Resource name and refined search styles.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,6 +2,8 @@ class Resource < ActiveRecord::Base
 
 	attr_accessible :name, :author, :craft_id, :avatar, :description
 
+	validates :name, :uniqueness => true
+
 	has_attached_file :avatar, :styles => { :medium => "255x255>", :thumb => "100x100>" }, :default_url => "/assets/eloquent.jpg"
 
 	belongs_to :crafts
