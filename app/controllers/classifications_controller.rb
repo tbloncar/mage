c624bcba3384f8a8fe61e21ecd838f5f111af44b
Travis Loncar <loncar.travis@gmail.com>
2013-04-30 00:58:40 -0500

    Refine ClassificationsController and edit show view.

--- app/controllers/classifications_controller.rb
+++ app/controllers/classifications_controller.rb
@@ -2,6 +2,15 @@ class ClassificationsController < ApplicationController
 
 	def show
 		@classification = Classification.find_by_path(params[:classification_path])
+		@crafts = @classification.crafts
+		@resources = []
+		@crafts.each do |craft|
+			craft.resources.each do |craft_resource|
+				@resources.push(craft_resource)
+			end
+		end
+		@resources
+
 	end
 
 end
