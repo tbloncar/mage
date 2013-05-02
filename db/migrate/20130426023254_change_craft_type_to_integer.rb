18c6c6e6e25e024b01065e64d0be0c7ed07ad016
Travis Loncar <loncar.travis@gmail.com>
2013-05-02 18:24:54 -0500

    Change migration for PG.

--- db/migrate/20130426023254_change_craft_type_to_integer.rb
+++ db/migrate/20130426023254_change_craft_type_to_integer.rb
@@ -1,6 +1,7 @@
 class ChangeCraftTypeToInteger < ActiveRecord::Migration
   def up
-  	change_column :resources, :craft, :integer, :default => 0, :null => false
+  	change_column :resources, :craft, :fixnum, :default => 0, :null => false
+    change_column :resources, :craft, :integer, :default => 0, :null => false
   end
 
   def down
