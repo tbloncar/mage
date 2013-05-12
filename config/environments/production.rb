d57529f5d5fd1ac802b177b636655b0372265434
Travis Loncar <loncar.travis@gmail.com>
2013-05-12 10:38:53 -0500

    Configure production environment for IMGKit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,8 @@ Mage::Application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  IMGKit.configure do |config|
+    config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
+  end
 end
