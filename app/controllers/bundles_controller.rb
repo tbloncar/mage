35bd9816941b579b60287a9612fa2e44046c93f4
Travis Loncar <loncar.travis@gmail.com>
2013-05-15 00:48:12 -0500

    Set up bundle show view.

--- app/controllers/bundles_controller.rb
+++ app/controllers/bundles_controller.rb
@@ -7,7 +7,25 @@ class BundlesController < ApplicationController
   end
 
   def create
+    @bundle = Bundle.new(params[:bundle])
+    @bundle.user_id = current_user.id
+    @bundle.path = @bundle.name.downcase.gsub(" ", "-")
+    @bundle.save
 
+    resources = params[:resources]
+    temp_array = resources.split('|')
+    resources_array = []
+    temp_array.each do |number|
+      number != '' && !resources_array.include?(number) && resources_array.size < 6 ? (resources_array << number) : ()
+    end
+    resources_array.each do |id_resource|
+      inclusion = Inclusion.new
+      inclusion.bundle_id = @bundle.id
+      inclusion.resource_id = id_resource
+      inclusion.save
+    end
+
+    redirect_to bundle_url(@bundle.path)
   end
 
   def index
@@ -15,7 +33,7 @@ class BundlesController < ApplicationController
   end
 
   def show
-
+    @bundle = Bundle.find_by_path(params[:bundle_path])
   end
 
   def edit
