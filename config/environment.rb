10f713a953bb1b884ffa5fe0c9e4344e6352f035
Travis Loncar <loncar.travis@gmail.com>
2013-05-20 18:53:18 -0500

    Another test for IMGKit in production.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Mage::Application.initialize!
+
+IMGKit.configure do |config|
+  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
+end
